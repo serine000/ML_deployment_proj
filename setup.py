a6399ff7146c1382db9cfc5149fa902587903f7e
serine000 <serine_md@hotmail.com>
2023-05-08 06:04:30 +0300

    Build setup

--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT="-e ."
 def get_requirements(file_path: str) -> List:
-    """Fetch the necessary list of requirements"""
+    """Fetch the necessary list of requirements from the specified file path."""
 
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
 
 
 setup(
@@ -19,7 +25,7 @@ setup(
     author="Serine",
     author_email="serine_md@hotmail.com",
     packages=find_packages(),
-    install_requires=get_requirements(),
+    install_requires=get_requirements("requirements.txt"),
 
 )
 
