dee52c29b7357674f62733997c1fb569ba0ca2d2
serine000 <serine_md@hotmail.com>
2023-05-08 20:55:22 +0300

    Testing logging & exception

--- src/exception.py
+++ src/exception.py
@@ -15,9 +15,10 @@ def CustomException (Exception):
     """Return a custom user specific exception."""
 
     def __init__(self, error_message, error_details: sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_details)
     
 
     def __str__():
         return self.error_message
+
--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
-LOG_PATH_FILE = ps.path.join(logs_path, LOG_FILE)
+LOG_PATH_FILE = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename =  LOG_PATH_FILE,
